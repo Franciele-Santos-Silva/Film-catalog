ec12b3d28c2f8465e313bcbbe014f0c4079614cc
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-13 11:29:52 -0300

    Espaço entre container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <nav id="navbar">
       <h2>
         <Link to="/">
-          <BiCameraMovie /> MoviesLib
+          <BiCameraMovie /> FilmNest
         </Link>
       </h2>
       <form onSubmit={handleSubmit}>
